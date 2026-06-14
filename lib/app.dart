8b16af96cd96aefb527aadf356ea0c9ebeaeea08
Cristian Radulescu <kristianradulescu@gmail.com>
2026-06-14 10:16:58 +0200

    fix: entry point env loading

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:sessionbloom_desktop/extensions/extensions.dart';
 import 'package:sessionbloom_desktop/screens/signup_screen.dart';
-import 'screens/home_screen.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:sessionbloom_desktop/l10n/app_localizations.dart';
 
 class SessionBloom extends StatelessWidget {
   const SessionBloom({super.key});
-
-  static String get apiUrl => dotenv.env['API_URL'] ?? 'http://localhost:5000';
-
-  static bool get isProduction => dotenv.env['PRODUCTION'] == 'true';
 
   @override
   Widget build(BuildContext context) {
@@ -23,12 +16,12 @@ class SessionBloom extends StatelessWidget {
         GlobalCupertinoLocalizations.delegate
       ],
       supportedLocales: [Locale('en'), Locale('it')],
-      home: SignupScreen(),
       title: "SessionBloom",
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
         useMaterial3: true,
       ),
+      home: const SignupScreen(),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:sessionbloom_desktop/utils/config.dart';
 import "app.dart";
-import 'package:flutter/rendering.dart';
 
 void main() async {
-  // debugPaintSizeEnabled = true;
   WidgetsFlutterBinding.ensureInitialized();
 
   try {
