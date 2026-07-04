18dfc0d10173ddb08fd8f6e0f1abc21562ca0b50
Cristian Radulescu <kristianradulescu@gmail.com>
2026-07-04 10:27:29 +0200

    feat: registration flow

--- lib/screens/auth/code_screen.dart
+++ lib/screens/auth/code_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sessionbloom_desktop/screens/home_screen.dart';
 import 'package:sessionbloom_desktop/services/auth/auth_service.dart';
 import 'package:sessionbloom_desktop/services/api_error.dart';
 import 'package:sessionbloom_desktop/widgets/layouts/layout.dart';
@@ -6,6 +7,7 @@ import 'package:sessionbloom_desktop/widgets/auth/code_form.dart';
 
 class CodeScreen extends StatefulWidget {
   final String email;
+
   const CodeScreen({super.key, required this.email});
 
   @override
@@ -25,18 +27,15 @@ class _CodeScreenState extends State<CodeScreen> {
   @override
   void initState() {
     super.initState();
-    _startAuthFlow();
+    _startVerification();
   }
 
-  Future<void> _startAuthFlow() async {
-    setState(() => _isLoading = true);
+  Future<void> _startVerification() async {
+    setState(() { _isLoading = true; });
     try {
       final flowId = await _authService.getAuthFlow();
-      print(flowId);
-      setState(() {
-        _flowId = flowId;
-      });
-
+      setState(() { _flowId = flowId; });
+      await _authService.sendCode(flowId: _flowId, email: widget.email);
     } on ApiError catch (e) {
       setState(() => _errorMessage = e.message);
     } finally {
@@ -47,36 +46,34 @@ class _CodeScreenState extends State<CodeScreen> {
   Future<void> _handleCode() async {
     if (!_formKey.currentState!.validate()) return;
     setState(() {
-      _isLoading = true;
       _errorMessage = null;
     });
     try {
       await _authService.verifyCode(
-          flowId: _flowId,
-          code: _codeController.text.trim()
+        flowId: _flowId,
+        code: _codeController.text.trim(),
       );
       if (!mounted) return;
-      ScaffoldMessenger.of(context)
-          .showSnackBar(const SnackBar(content: Text("Code sent!")));
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Code sent for validation!")),
+      );
       Future.delayed(const Duration(seconds: 2), () {
-        if (mounted) Navigator.pop(context);
+        if (mounted) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (_) => const HomeScreen()),
+          );
+        }
       });
     } on ApiError catch (e) {
       setState(() {
         _errorMessage = e.message;
       });
-    } finally {
-      if (mounted) {
-        setState(() {
-          _isLoading = false;
-        });
-      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
-
     return AppLayout(
       child: Center(
         child: SingleChildScrollView(
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,65 +1,21 @@
 import 'package:flutter/material.dart';
-import '/widgets/home_body.dart';
-import '/services/api_health_check.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int _counter = 0;
-  Map<String, dynamic> _serverData = {};
-
-  @override
-  void initState() {
-    super.initState();
-    _checkConnection();
-  }
-
-  Future<void> _checkConnection() async {
-    final response = await ApiHealthCheck.testConnection();
-
-    setState(() {
-      // _serverData = response;
-    });
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Row(
-          children: [
-            const Text('SessionBloom'),
-            Text('Status: ${_serverData["body"] ?? "N/A"}'),
-            const SizedBox(width: 8),
-            Container(
-              width: 10,
-              height: 10,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: (_serverData["code"] == 200) ? Colors.green : Colors.red,
-              ),
-            ),
-          ],
-        )
-      ),
-      body: const HomeBody(),  // ← Even the body is extracted!
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Row(
+            children: [
+              const Text('SessionBloom'),
+              Text('OOOOOSNAF'),
+              const SizedBox(width: 8),
+            ],
+          )
       ),
     );
   }
